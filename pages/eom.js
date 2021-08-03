defa1f7ccfec30e4b8d2e2209ba6c3c5cef652d7
martin lowe <martinlowe673@gmail.com>
2021-08-03 07:08:53 +0100

    toolbar

--- pages/eom.js
+++ pages/eom.js
@@ -1,9 +1,12 @@
+import { StrategyValues } from "pako";
+import styles from "../styles/EOM.module.css";
+
 export const EOM = ({ employee }) => {
   return (
     <div className="page-container">
-      <div>
+      <div className={styles.main}>
         <h1>Employee of the Month</h1>
-        <div>
+        <div className={styles.employeeOfTheMonth}>
           <h3>{employee.name}</h3>
           <h6>{employee.position}</h6>
           <img src={employee.image} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import { Toolbar } from "../components/toolbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <div className="page-container">
+      <Toolbar />
+      <div className={styles.main}>
+        <h1>Martin News App</h1>
+        <h3>Your place to go for news app</h3>
+      </div>
+    </div>
+  );
 }
